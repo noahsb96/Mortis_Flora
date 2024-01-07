2c73290e756f7f9ac779a1568bbdbad6034f76ef
noahsb96 <noahsb96@yahoo.com>
2024-01-07 15:50:57 -0700

    styling nav a bit more

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@ import Home from './components/Home.vue'
 
 <template>
   <header class="bg-[#EBEEDD]">
-    <img src="./logos/IMG_1932.png" class="w-64" />
+    <div class="flex flex-wrap flex-row justify-between align-middle">
+      <img src="./logos/IMG_1932.png" class="w-64 ml-40 " />
+      <RouterLink to="/quote" class="float-right mr-72 mt-auto mb-auto border-2 border-[#A94802] p-3 bg-[#A94802] rounded-full font-bold">Get a Quote!</RouterLink>
+    </div>
     <div class="pb-6">
-      <nav class="border-b-2 border-black flex items-center justify-center space-x-8">
+      <nav class="border-b-2 border-[#5D612E] flex items-center justify-center space-x-8">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/quote">Quote</RouterLink>
         <RouterLink to="/pictures">Pictures</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/contact">Contact</RouterLink>
