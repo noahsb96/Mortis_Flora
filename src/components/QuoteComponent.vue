cc792012e722c2e395a1a01b2402057450d89b77
noahsb96 <noahsb96@yahoo.com>
2024-01-14 13:03:03 -0700

    Added select tag on Terrarium form for people to choose options

--- src/components/QuoteComponent.vue
+++ src/components/QuoteComponent.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1 class="text-center text-5xl font-bold pt-5">Terrarium Order Form</h1>
-  <div class="mx-64 px-10 py-5 mt-10 bg-white">
+  <div class="mx-64 px-10 py-5 mt-10 bg-white mb-10">
     <p>
       <label class="text-2xl font-bold">Your Name:</label>
       <br />
@@ -34,6 +34,16 @@
           placeholder="Your Phone Number"
           class="border-2 border-[#d2d2d2] w-full text-black rounded-md pl-4 font-bold"
         />
+      </span>
+    </p>
+    <p class="pt-5">
+      <label class="text-2xl font-bold">Enclosure Type:</label>
+      <br />
+      <span>
+        <select class="border-2 border-[#d2d2d2] w-full text-black rounded-md pl-4 font-bold">
+          <option value="Glass Dome">Glass Dome</option>
+          <option value="Glass Cube">Glass Cube</option>
+        </select>
       </span>
     </p>
   </div>
