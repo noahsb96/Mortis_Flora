d903e56953f20fd00efb9fb55cb186e279c7fc48
noahsb96 <noahsb96@yahoo.com>
2024-01-13 22:20:56 -0700

    Creating form for terrarium order form

--- src/components/QuoteComponent.vue
+++ src/components/QuoteComponent.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-center text-2xl font-bold">Terrarium Order Form</h1>
+  <h1 class="text-center text-5xl font-bold pt-5">Terrarium Order Form</h1>
+  <div class="mx-64 px-10 mt-10 bg-white">
+    <p class="py-5">
+      <label class="text-2xl font-bold">Your Name:</label>
+      <br>
+      <span>
+        <input type="text" placeholder="Your Name" class="border-2 border-[#d2d2d2] w-full text-black rounded-md pl-4 font-bold" />
+      </span>
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
