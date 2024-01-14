f228fa499abd27ed843791cbce43387356395e90
noahsb96 <noahsb96@yahoo.com>
2024-01-13 21:39:34 -0700

    Added some hover effects to buttons and link around image to home

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,25 @@ import { RouterLink, RouterView } from 'vue-router'
   <header class="bg-[#EBEEDD]">
     <div class="w-screen">
       <div class="pl-10 pr-10 font-bold text-2xl pt-2">
-        <RouterLink to="/contact" class="float-right mr-64">Contact</RouterLink>
-        <RouterLink to="/about" class="mr-6 float-right">About</RouterLink>
-    </div>
+        <RouterLink
+          to="/contact"
+          class="py-2 px-4 float-right mr-64 hover:bg-black hover:text-white rounded-full"
+          >Contact</RouterLink
+        >
+        <RouterLink
+          to="/about"
+          class="py-2 px-4 mr-6 float-right hover:bg-black hover:text-white rounded-full"
+          >About</RouterLink
+        >
+      </div>
     </div>
     <div class="flex flex-wrap flex-row justify-between align-middle w-screen">
-      <img src="./logos/IMG_1932.png" class="w-64 ml-40" />
+      <RouterLink to="/">
+        <img src="./logos/IMG_1932.png" class="w-64 ml-40" />
+      </RouterLink>
       <RouterLink
         to="/quote"
-        class="float-right mr-72 mt-auto mb-auto border-2 border-[#5D612E] px-4 py-3 bg-[#5D612E] rounded-full font-bold text-[#EBEEDD] text-2xl"
+        class="float-right mr-72 mt-auto mb-auto border-2 border-[#5D612E] px-4 py-3 bg-[#5D612E] rounded-full font-bold text-[#EBEEDD] text-2xl hover:bg-black hover:text-white hover:border-black"
         >Quote Order Form</RouterLink
       >
     </div>
@@ -22,8 +32,14 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav
         class="border-b-2 border-[#5D612E] flex items-center justify-center space-x-28 font-bold text-5xl"
       >
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/pictures">Pictures</RouterLink>
+        <RouterLink to="/" class="py-2 px-4 text-black hover:bg-black hover:text-white rounded-full"
+          >Home</RouterLink
+        >
+        <RouterLink
+          to="/pictures"
+          class="py-2 px-4 text-black hover:bg-black hover:text-white rounded-full"
+          >Pictures</RouterLink
+        >
       </nav>
     </div>
   </header>
