a31ce2fde75bffb7a7ada3fb98e633c60661b937
noahsb96 <noahsb96@yahoo.com>
2024-02-06 23:23:01 -0700

    Added partners in about. I will create a contact form and a new emailjs template for emails to be sent to

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,31 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-center text-6xl font-bold pt-5 font-amatic">Death is never the end of the journey</h1>
+  <h1 class="text-center text-6xl font-bold pt-5 font-amatic">
+    Death is never the end of the journey
+  </h1>
   <div class="mx-64 px-10 py-5 mt-10 bg-[#A94802]">
-    <h2 class="text-2xl font-bold text-center">Just your average guy with a house full of animals and dead things.</h2>
+    <h2 class="text-2xl font-bold text-center">
+      ☠️ Just your average guy with a house full of animals and dead things ☠️
+    </h2>
     <p class="text-justify">
-    When I was a kid, I always
-    found myself collecting assortments of shells and feathers or “trash treasures”, as my mother
-    used to call them. This passion bled into my academic days, where I longed for a degree in
-    Zoology to further foster my interest in the spectacle that is fauna biodiversity. After that
-    great adventure was completed and satisfied, I spent a few years as a horticulturist and florist
-    for a few small companies. It goes without saying that plants and animals both perish at the end
-    of their allotted lifespans, but I never understood why the story has to end there.
-</p>
-    <p class="text-justify">With my
-    newfound education equipped, (I have a big fancy degree that means I can tell you the difference
-    between a lion and a camel skull, and as an added bonus, I know what a petunia is!) I began to
-    dust off those old trash treasures and find better ways of displaying them, combining pieces
-    together behind geometric glass displays so that I could enjoy their presence in a more “adult”
-    fashion.</p>
-    <p class="text-justify">With an unwavering passion for both artistry and conservation, my creations stand as
-    exquisite tributes to the splendor of nature and the elegance of design. Inspired by the
-    timeless allure of museum dioramas, each piece is meticulously crafted to ignite the
-    imagination. Embracing ethical stewardship, I meticulously source materials from sustainable and
-    respectful origins, ensuring that every creation resonates with a profound reverence for the
-    natural world.
-  </p>
+      When I was a kid, I always found myself collecting assortments of shells and feathers or
+      “trash treasures”, as my mother used to call them. This passion bled into my academic days,
+      where I longed for a degree in Zoology to further foster my interest in the spectacle that is
+      fauna biodiversity. After that great adventure was completed and satisfied, I spent a few
+      years as a horticulturist and florist for a few small companies. It goes without saying that
+      plants and animals both perish at the end of their allotted lifespans, but I never understood
+      why the story has to end there.
+    </p>
+    <p class="text-justify">
+      With my newfound education equipped, (I have a big fancy degree that means I can tell you the
+      difference between a lion and a camel skull, and as an added bonus, I know what a petunia is!)
+      I began to dust off those old trash treasures and find better ways of displaying them,
+      combining pieces together behind geometric glass displays so that I could enjoy their presence
+      in a more “adult” fashion.
+    </p>
+    <p class="text-justify">
+      With an unwavering passion for both artistry and conservation, my creations stand as exquisite
+      tributes to the splendor of nature and the elegance of design. Inspired by the timeless allure
+      of museum dioramas, each piece is meticulously crafted to ignite the imagination. Embracing
+      ethical stewardship, I meticulously source materials from sustainable and respectful origins,
+      ensuring that every creation resonates with a profound reverence for the natural world.
+    </p>
+  </div>
+  <h1 class="text-center text-6xl font-bold pt-5 font-amatic">Check out our Partners!</h1>
+  <div class="mx-64 px-10 py-5 mt-10 mb-20 bg-[#A94802]">
+    <a href="https://thebayoubee.etsy.com/">The Bayou Bee</a>
+    <br>
+    <a href="https://www.leonardcrystalsco.com/">Leonard Crystal Co</a>
   </div>
 </template>
 
